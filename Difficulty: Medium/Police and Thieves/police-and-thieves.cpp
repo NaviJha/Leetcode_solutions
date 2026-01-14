c26638bc956ea1e0bbe849e77945522e3867a4f8
Vaishnavi jha <155554991+NaviJha@users.noreply.github.com>
2026-01-14 19:01:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    int catchThieves(vector<char> &arr, int k) {
+        // Code here
+           int n = arr.size();
+        int m = k+1;
+        int count = 0;
+        deque<int>police;
+        deque<int>thief;
+            
+        for(int i=0;i<n;i++){
+            if(arr[i]=='P'){
+                if(thief.empty()){
+                    police.push_back(i);
+                }
+                else{
+                    while(!thief.empty() && i-thief.front()>k){
+                        thief.pop_front();
+                    }
+                    if(!thief.empty()){
+                        thief.pop_front();
+                        count++;
+                    }
+                }
+            }
+            else{
+                if(police.empty()){
+                    thief.push_back(i);
+                }
+                else{
+                    while(!police.empty() && i-police.front()>k){
+                        police.pop_front();
+                    }
+                    if(!police.empty()){
+                        police.pop_front();
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
