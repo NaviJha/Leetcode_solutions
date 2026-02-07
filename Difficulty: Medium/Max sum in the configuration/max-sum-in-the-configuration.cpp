d90c82f88c1e4037854a7dcda13f0a658c8454a2
Vaishnavi jha <155554991+NaviJha@users.noreply.github.com>
2026-02-07 22:20:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max sum in the configuration/max-sum-in-the-configuration.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int maxSum(vector<int> &arr) {
+        int n = arr.size();
+        
+        int arrSum = 0;
+        int currVal = 0;
+
+        for (int i = 0; i < n; i++) {
+            arrSum += arr[i];
+            currVal += i * arr[i];
+        }
+
+        int maxVal = currVal;
+
+        // Compute values for all rotations
+        for (int i = 1; i < n; i++) {
+            currVal = currVal + arrSum - n * arr[n - i];
+            maxVal = max(maxVal, currVal);
+        }
+
+        return maxVal;
+    }
+};
