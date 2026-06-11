773272c2ab2ea592996ddc93f723c59509ba4dad
Vaishnavi jha <155554991+NaviJha@users.noreply.github.com>
2026-06-11 20:53:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equal Point in Brackets/equal-point-in-brackets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findIndex(string &s) {
+        int n = s.size();
+
+        int close = 0;
+        for(char ch : s) {
+            if(ch == ')') close++;
+        }
+
+        int open = 0;
+
+        for(int i = 0; i <= n; i++) {
+
+            if(open == close)
+                return i;
+
+            if(i < n) {
+                if(s[i] == '(')
+                    open++;
+                else
+                    close--;
+            }
+        }
+
+        return -1;
+    }
+};
