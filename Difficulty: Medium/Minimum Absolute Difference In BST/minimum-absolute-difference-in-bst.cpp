0258c84c2d9ca6a6f14946b614b140f7d73ea354
Vaishnavi jha <155554991+NaviJha@users.noreply.github.com>
2026-09-18 09:37:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Absolute Difference In BST/minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void solve(Node* root,int &a,int &mini){
+        if(root == NULL){
+            return ;
+        }
+        solve(root->left,a,mini);
+        if(a != -1){
+          mini = min(mini,root->data-a);
+        }
+        a = root->data;
+        solve(root->right,a,mini);
+        return ;
+    }
+    int absDiff(Node *root) {
+        int a = -1;
+        int mini = INT_MAX;
+        solve(root,a,mini);
+        return mini;
+    }
+};
